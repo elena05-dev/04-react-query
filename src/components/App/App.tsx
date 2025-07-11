f5ca6eca59e1e49f7fd939c183bc744979c10ca6
Elena Polyakova <epolyk1970@gmail.com>
2025-07-11 15:54:50 +0300

    paginate

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import toast, { Toaster } from "react-hot-toast";
-import ReactPaginate from "../ReactPaginate/ReactPaginate";
+import ReactPaginateLib from "react-paginate";
 import SearchBar from "../SearchBar/SearchBar";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import Loader from "../Loader/Loader";
@@ -10,6 +10,7 @@ import MovieModal from "../MovieModal/MovieModal";
 import type { Movie } from "../../types/movie";
 import { fetchMovies } from "../../services/movieService";
 import type { FetchMoviesResponse } from "../../services/movieService";
+import css from "./App.module.css";
 
 export default function App() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -46,21 +47,32 @@ export default function App() {
       <Toaster position="top-right" />
       <SearchBar onSubmit={handleSearch} />
       {isError && <ErrorMessage />}
-      {isLoading && !isFetching && <Loader />}{" "}
+      {isLoading && !isFetching && <Loader />}
+
       {data && (
         <>
-          <ReactPaginate
-            totalPages={data.total_pages}
-            currentPage={page}
-            onPageChange={(newPage) => {
-              setPage(newPage);
-              window.scrollTo({ top: 0, behavior: "smooth" });
-            }}
-          />
+          {data.total_pages > 1 && (
+            <ReactPaginateLib
+              pageCount={data.total_pages}
+              pageRangeDisplayed={5}
+              marginPagesDisplayed={1}
+              onPageChange={({ selected }) => {
+                setPage(selected + 1);
+                window.scrollTo({ top: 0, behavior: "smooth" });
+              }}
+              forcePage={page - 1}
+              previousLabel="←"
+              nextLabel="→"
+              renderOnZeroPageCount={null}
+              containerClassName={css.pagination}
+              activeClassName={css.active}
+            />
+          )}
 
           <MovieGrid movies={data.results} onSelect={setSelectedMovie} />
         </>
       )}
+
       {selectedMovie && (
         <MovieModal
           movie={selectedMovie}
